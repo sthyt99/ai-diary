9d188351617efd968f7c213ea3ab147734dd613b
sthyt99 <170448040@ccalumni.meijo-u.ac.jp>
2025-09-29 21:10:34 +0900

    初回コミット

---
+++ src/main/java/com/example/ai_diary/backend/AiDiaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_diary.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDiaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDiaryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ai_diary/backend/AiDiaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_diary.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDiaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
