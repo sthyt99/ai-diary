b5e19455120a4c98cc2087242b5d0b7fdeb87568
sthyt99 <170448040@ccalumni.meijo-u.ac.jp>
2025-10-31 00:41:40 +0900

    PostgreSQLをDockerで起動する処理を実装(#13)

--- src/main/java/com/example/ai_diary/backend/AiDiaryBackendApplication.java
+++ src/main/java/com/example/ai_diary/backend/AiDiaryBackendApplication.java
@@ -1,3 +1,4 @@
+// src/main/java/com/example/ai_diary/backend/AiDiaryBackendApplication.java
 package com.example.ai_diary.backend;
 
 import org.springframework.boot.SpringApplication;
@@ -5,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AiDiaryBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AiDiaryBackendApplication.class, args);
 	}
-
 }
